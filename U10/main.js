61aaa986664663e639f872519fab06ca5acf2c23
Axel <axelberdugo@gmail.com>
2016-06-28 15:45:27 +0100

    PreWork Upload

---
+++ U10/main.js
@@ -0,0 +1,19 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+
+if (cardOne===cardTwo) {
+  alert ("You found a match !")
+} else if (cardThree===cardFour) {
+  alert ("You found a match !")
+}  else if (cardOne===cardThree) {
+  alert ("Sorry try again")
+} if (cardOne===cardFour) {
+  alert ("Sorry try again")
+} 
+
+var createBoard = function
+var createBoard = document.createElement('section');
+
+document.getElementById('trogdor');
