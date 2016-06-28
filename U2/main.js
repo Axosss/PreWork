6899f540b53179f5db58e0f25ba08034556e0cd9
Axel <axelberdugo@gmail.com>
2016-06-28 15:51:22 +0100

    Stash

---
+++ U2/main.js
@@ -0,0 +1,15 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+
+if (cardOne===cardTwo) {
+  alert ("You found a match !")
+} else if (cardThree===cardFour) {
+  alert ("You found a match !")
+}  else if (cardOne===cardThree) {
+  alert ("Sorry try again")
+} if (cardOne===cardFour) {
+  alert ("Sorry try again")
+} 
+
